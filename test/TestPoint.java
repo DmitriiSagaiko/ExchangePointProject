24210c4691b8ae71ad79c562c54af77bb1af7f98
Mihail Nedioglo <nmg84nmg@gmail.com>
2023-11-29 20:59:20 +0100

    update

---
+++ test/TestPoint.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class TestPoint {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+    @Test
+    void setStr() {
+
+    }
+
+
+
+
+}
